6feed0daceee4eb001794186d90b788eee12a83c
Artem Gorbach <gorbacartem90@gmail.com>
2020-03-12 17:57:36 +0200

    add tab bottom navigator

--- src/navigation/MainStackNavigator.tsx
+++ src/navigation/MainStackNavigator.tsx
@@ -8,8 +8,41 @@ import SignUp from "../screens/auth/SignUp";
 import Main from "../screens/Main";
 // hooks
 import { useAuth } from "../hooks/useAuth";
+// tab navigation
+import { Ionicons } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createStackNavigator();
+
+const Tab = createBottomTabNavigator();
+
+const Test = () => (
+  <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+
+        if (route.name === "Home") {
+          iconName = focused
+            ? "ios-information-circle"
+            : "ios-information-circle-outline";
+        } else if (route.name === "Settings") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        }
+
+        // You can return any component that you like here!
+        return <Ionicons name={iconName} size={size} color={color} />;
+      }
+    })}
+    tabBarOptions={{
+      activeTintColor: "tomato",
+      inactiveTintColor: "black"
+    }}
+  >
+    <Tab.Screen name="Home" component={SignUp} />
+    <Tab.Screen name="Settings" component={Main} />
+  </Tab.Navigator>
+);
 
 const MainStackNavigator = () => {
   const { user } = useAuth();
@@ -31,6 +64,7 @@ const MainStackNavigator = () => {
           <Stack.Screen name="Main" component={Main} />
         )}
       </Stack.Navigator>
+      <Stack.Screen name="Test" component={Test} />
     </NavigationContainer>
   );
 };
